8ef50013a5b28753b5b767e98a75a08a7a698c24
Minjoo <m0522inju@gmail.com>
2024-08-09 00:35:20 +0900

    init

---
+++ src/main/java/hello/dockercomposecdpractice/DockerComposeCdPracticeApplication.java
@@ -0,0 +1,13 @@
+package hello.dockercomposecdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeCdPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerComposeCdPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/dockercomposecdpractice/DockerComposeCdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.dockercomposecdpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeCdPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
